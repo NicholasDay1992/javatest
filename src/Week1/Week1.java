e0847f55456b54a871c6aaa83cb4231b160c9e14
Nicholas Day <71455485+NicholasDay1992@users.noreply.github.com>
2022-10-06 08:49:41 +0100

    reset W1

--- src/Week1/Week1.java
+++ src/Week1/Week1.java
@@ -16,38 +16,14 @@ public class Week1
         System.out.println();
         System.out.println(" by Nicholas Day and Derek Peacock");
         System.out.println();
-/* 
+
         String name = InputReader.getString("Enter your name > ");
         System.out.println("Hello " + name);
         System.out.println("nice to meet you!");
 
         int id = InputReader.getInt("Enter your id > ");
         System.out.println("Your ID is " + id);
-*/
 
 
-     // THIS IS EXERCISE 3: 
-        int length = InputReader.getInt("Enter length: ");
-        int height = InputReader.getInt("Enter height: ");
-
-        int perimeter = (length + height) * 2;
-        int area = length * height;
-
-        System.out.println("The area is " + area);
-        System.out.println("The perimter is: " + perimeter);
-
-        
-
-        //EXERCISE 4:
-/* 
-        int num1 = InputReader.getInt("Enter first number: ");
-        int num2 = InputReader.getInt("Enter second number: ");
-
-        int average = (num1 + num2) / 2;
-
-        System.out.println("Average of " + num1 + " and " + num2 + " is " + average);
-
-
-*/
     } 
 }
